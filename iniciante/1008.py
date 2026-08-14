5b8e11a436393012a64561323bb43c6aee693a0e
Bruno Felipe <brunofelipecoder@gmail.com>
2026-08-13 21:49:29 -0300

    feat/adicionando mais 3 exercicios

---
+++ iniciante/1008.py
@@ -0,0 +1,6 @@
+idFunc = int(input())
+horasTrabalhadas = int(input())
+pgmtHora = float(input())
+
+print(f"NUMBER = {idFunc}")
+print("SALARY = U$ {:.2f}".format(horasTrabalhadas * pgmtHora))
---
+++ iniciante/1009.py
@@ -0,0 +1,5 @@
+nomeVendedor = str(input())
+salarioVendedor = float(input())
+totalVendas = float(input())
+
+print("TOTAL = R$ {:.2f}".format(salarioVendedor + (totalVendas * 0.15)))
